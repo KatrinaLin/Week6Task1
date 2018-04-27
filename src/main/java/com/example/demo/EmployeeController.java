8be3b173e7ee3aeb5caa39b2965e8a85751286af
Katrina <zhaoyanlin2016@gmail.com>
2018-04-27 09:57:18 +0800

    Implement the Get method to retrieve list of employees. Implement the Post method to add new employee.

--- src/main/java/com/example/demo/EmployeeController.java
+++ src/main/java/com/example/demo/EmployeeController.java
@@ -1,11 +1,32 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.*;
 
 @RestController
 @RequestMapping(value = "/employees")
 public class EmployeeController {
 
+    static Map<Long, Employee> employees = Collections.synchronizedMap(new HashMap<Long, Employee>());
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Employee> getEmployeeList() {
+
+        List<Employee> result = new ArrayList<Employee>(employees.values());
+
+        return result;
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public String addEmployee(@ModelAttribute Employee employee) {
+
+        employees.put(employee.getId(), employee);
+
+        return "Added Employee";
+    }
 
 }
