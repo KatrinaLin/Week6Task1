e69ed944b02dafe2ecf62cedefb1c977903e719f
Katrina <zhaoyanlin2016@gmail.com>
2018-04-27 08:57:50 +0800

    Create Employee class.

---
+++ src/main/java/com/example/demo/Employee.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+public class Employee {
+    private long id;
+    private String name;
+    private int age;
+    private String gender;
+
+    public Employee(long id, String name, int age, String gender) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+}
